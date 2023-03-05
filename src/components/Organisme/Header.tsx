b17bcb8e8dd90c7c590e08f7f19fe6b615c1cc7a
alifsyahakbar <alifsyah789@gmail.com>
2023-03-05 16:25:05 +0700

    remove rounded header

--- src/components/Organisme/Header.tsx
+++ src/components/Organisme/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({ HandleSearch }: any) {
   return (
     <div className="">
       <Link href="/">
-        <div className="flex justify-center w-full pt-2 hover:bg-[#484197] bg-[#484197] rounded-b-3xl">
+        <div className="flex justify-center w-full pt-2 hover:bg-[#484197] bg-[#484197]">
           <h1 className="relative flex drop-shadow-xl font-bangers text-4xl lg:text-5xl text-white text-center my-4 dark:text-white">
             Moshidesu<div className="animate-[bounce_1.5s_infinite]">.</div>Moe
             <Image
